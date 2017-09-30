0d52d73048d45246d7ca89d6ec9efe04b00e9b45
Pedro Elpidio Melha Lemos <pedroemlemos@gmail.com>
2017-09-29 23:29:36 -0300

    Refactoring and getting the code ready to merge

--- BusyWaitMutex/main.c
+++ BusyWaitMutex/main.c
@@ -7,35 +7,28 @@
 #define NUM_WRITERS 4
 #define DATA_SIZE 30
 
-char* dado;
-int pos;
-Mutex* mutexEscritores;
-int posLeitoresMutex=NUM_WRITERS;
+char* data;
+int last_char_index;
+Mutex* mutex;
+int mutex_readers_index = NUM_WRITERS;
 
-
-void appendChar(char** string, int* pos, char c) {
-    (*string)[*pos]=c;
-    (*string)[(*pos)+1] = '\0';
-    (*pos)++;
+void append_char(char **string, int i, char c) {
+    (*string)[i] = c;
+    (*string)[i+1] = '\0';
 }
 
 void* writer(void* param) {
     int* thread_number;
     thread_number = param;
 
-    
-
     if (*thread_number >= 0 && *thread_number < NUM_WRITERS) { // If it has one digit
         char str[2];
         sprintf(str, "%d", *thread_number);
-        lock(mutexEscritores, *thread_number);
-
-        printf("Thread Escritora %d escrevendo\n", *thread_number);
-
-        appendChar(&dado, &pos, str[0]);
-        printf("toString: %s\n", dado);
-
-        unlock(mutexEscritores, *thread_number);
+        lock(mutex, *thread_number);
+        printf("Thread Writer %d writing...", *thread_number);
+        append_char(&data, last_char_index++, str[0]);
+        printf(" \"%s\"\n", data);
+        unlock(mutex, *thread_number);
     }
 
     free(param);
@@ -49,60 +42,64 @@ void* reader(void* param) {
     int* thread_number;
     thread_number = param;
 
-    if (*thread_number>=0 && *thread_number<NUM_READERS) {
-        lock(mutexEscritores, posLeitoresMutex);
+    if (*thread_number >= 0 && *thread_number < NUM_READERS) {
+        lock(mutex, mutex_readers_index);
+        printf("Thread Reader %d reading: \"%s\"\n", *thread_number, data);
+        unlock(mutex, mutex_readers_index);
+    }
 
-        printf("Thread Leitora %d lendo - %s\n", *thread_number, dado);
+    free(param);
 
-        unlock(mutexEscritores, posLeitoresMutex);
-        
+    pthread_exit(NULL);
+
+    return NULL;
+}
+
+int create_threads(bool is_writer, pthread_t* threads, int length) {
+    int err_code;
+
+    for (int i = 0; i < length; i++) {
+        printf(is_writer ? "Writer %d has been created!\n" : "Reader %d has been created!\n", i);
+        int* thread_number = (int*) malloc(sizeof(int));
+        *thread_number = i;
+
+        err_code = pthread_create((threads + i), NULL, is_writer ? writer : reader, (void*) thread_number);
+
+        if (err_code) {
+            printf("ERROR code is %d\n", err_code);
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+void join_all(pthread_t* threads, int length) {
+    for (int i = 0; i < length; i++) {
+        pthread_join(*(threads+i), NULL);
     }
 }
 
-
 int main() {
-    dado = (char*)malloc(sizeof(char)*DATA_SIZE);
-    pos = 0;
+    data = (char*)malloc(sizeof(char)*DATA_SIZE);
+    last_char_index = 0;
 
-    pthread_t leitores[NUM_READERS];
-    pthread_t escritores[NUM_WRITERS];
-    mutexEscritores = malloc_mutex(NUM_WRITERS);
+    pthread_t readers[NUM_READERS];
+    pthread_t writers[NUM_WRITERS];
+    mutex = malloc_mutex(NUM_WRITERS);
 
-    int err_code;
+    int err_code_writers, err_code_readers;
 
-    for(int i = 0; i < NUM_WRITERS; i++) {
-        printf("escritor %d\n", i);
-        int* thread_number = (int*) malloc(sizeof(int));
-        *thread_number = i;
-        err_code = pthread_create(&escritores[i], NULL, writer, (void*) thread_number);
+    err_code_writers = create_threads(true, &(writers[0]), NUM_WRITERS);
+    err_code_readers = create_threads(false, &(readers[0]), NUM_READERS);
 
-        if (err_code){
-            printf("ERROR code is %d\n", err_code);
-            return -1;
-        }
-    }
+    if (err_code_writers != 0 || err_code_readers != 0)
+        return -1;
 
-    for(int i = 0; i < NUM_READERS; i++) {
-        printf("leitor %d\n", i);
-        int* thread_number = (int*) malloc(sizeof(int));
-        *thread_number = i;
-
-        err_code = pthread_create(&leitores[i], NULL, reader, (void*) thread_number);
-
-        if (err_code){
-            printf("ERROR code is %d\n", err_code);
-            return -1;
-        }
-    }
-
-
-
-    for (int i = 0; i < NUM_WRITERS; i++) {
-        pthread_join(escritores[i], NULL);
-    }
+    join_all(&(writers[0]), NUM_WRITERS);
+    join_all(&(readers[0]), NUM_READERS);
 
     printf("Thread main finished\n");
-    pthread_exit(NULL);
 
     return 0;
 }
